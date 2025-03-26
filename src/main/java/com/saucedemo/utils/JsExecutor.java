dc50d3a672c2c2021a21418e64aaa422aea3cd9c
Luciano <lof.fagundes@gmail.com>
2025-03-26 14:43:06 -0300

    fix: Unchecked cast na JSE e metodo pageLoadTime.

--- src/main/java/com/saucedemo/utils/JsExecutor.java
+++ src/main/java/com/saucedemo/utils/JsExecutor.java
@@ -26,13 +26,13 @@ public class JsExecutor {
 
     public static void pageLoadTime(WebDriver driver, long timeLimitMs) {
         JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
-        long loadEventEnd = (long) jsExecutor.executeScript("return window.performance.timing.loadEventEnd;");
-        long navigationStart = (long) jsExecutor.executeScript("return window.performance.timing.navigationStart;");
-        if (loadEventEnd == 0) {
+        Number loadEventEnd = (Number) jsExecutor.executeScript("return window.performance.timing.loadEventEnd;");
+        Number navigationStart = (Number) jsExecutor.executeScript("return window.performance.timing.navigationStart;");
+        if (loadEventEnd == null || loadEventEnd.longValue() == 0) {
             Report.logCapture(Status.FAIL, "A página ainda não terminou de carregar ou 'loadEventEnd' não está acessível.");
             throw new IllegalStateException("'loadEventEnd' é zero. Não foi possível calcular o tempo de carregamento.");
         }
-        long loadTime = loadEventEnd - navigationStart;
+        long loadTime = loadEventEnd.longValue() - navigationStart.longValue();
         Report.logCapture(Status.INFO, "Tempo de carregamento da página: " + loadTime + "ms");
         if (loadTime > timeLimitMs) {
             String mensagemErro = String.format(
